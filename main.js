eb3ca29e4d1ddd5bd556d27504eb3396469c1098
박수민 <55694784+uzoo0724@users.noreply.github.com>
2023-02-13 23:12:25 +0900

    docs: main 수정

--- main.js
+++ main.js
@@ -19,8 +19,21 @@
 
 const express = require('express');
 const app = express();
+var fs = require('fs');
+var template = require('./lib/template.js');
 
-app.get('/', (req, res) => res.send('/'))
+app.get('/', (req, res) => {
+  fs.readdir('./data', function(err, filelist) {
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+                             `<h2>${title}</h2>${description}`,
+                             `a href="/create">create</a>`
+                             );
+    res.send(html);
+  });
+});
 app.get('/page', (req, res) => res.send('/page'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
