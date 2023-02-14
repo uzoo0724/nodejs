92f04648c079790088d7850b225e66b0f549c432
unknown <kid07118@naver.com>
2023-02-15 00:07:57 +0900

    chore: main 수정

--- main.js
+++ main.js
@@ -21,19 +21,45 @@ const express = require('express');
 const app = express();
 var fs = require('fs');
 var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 app.get('/', (req, res) => {
-  fs.readdir('./data', function(err, filelist) {
+  fs.readdir('./data', function (err, filelist) {
+    console.log(filelist);
     var title = 'Welcome';
     var description = 'Hello, Node.js';
     var list = template.list(filelist);
     var html = template.HTML(title, list,
-                             `<h2>${title}</h2>${description}`,
-                             `a href="/create">create</a>`
-                             );
+      `<h2>${title}</h2>${description}`,
+      `a href="/create">create</a>`
+    );
     res.send(html);
   });
 });
-app.get('/page', (req, res) => res.send('/page'))
+
+app.get('/page/:pageId', (req, res) => {
+  fs.readdir('./data', function (err, filelist) {
+    var filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+      var title = req.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        `<a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      res.send(html);
+    })
+  });
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
